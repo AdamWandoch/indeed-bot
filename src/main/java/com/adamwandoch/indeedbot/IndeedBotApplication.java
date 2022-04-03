610836d645aec63fc39a5c8515632d8438f86fca
Adam Wandoch <wandoch.adam@gmail.com>
2022-04-03 02:03:49 +0100

    heroku trial deploy

--- src/main/java/com/adamwandoch/indeedbot/IndeedBotApplication.java
+++ src/main/java/com/adamwandoch/indeedbot/IndeedBotApplication.java
@@ -1,10 +1,15 @@
 package com.adamwandoch.indeedbot;
 
+import com.adamwandoch.indeedbot.model.IndeedJob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class IndeedBotApplication {
+
+	public static List<IndeedJob> cachedJobs = SearchUtils.getAllActiveJobs();
 
 	public static void main(String[] args) {
 		SpringApplication.run(IndeedBotApplication.class, args);
--- src/main/java/com/adamwandoch/indeedbot/controller/MainController.java
+++ src/main/java/com/adamwandoch/indeedbot/controller/MainController.java
@@ -3,6 +3,8 @@ package com.adamwandoch.indeedbot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.adamwandoch.indeedbot.IndeedBotApplication.cachedJobs;
+
 /**
  * @author Adam Wandoch
  */
@@ -12,6 +14,23 @@ public class MainController {
 
     @GetMapping("/")
     public String home() {
-        return "<font size=5>Home Page</font>";
+        return "<font size=5>Home Page</font><br>Try using /jobs endpoint...";
+    }
+
+    @GetMapping("/jobs")
+    public String jobs() {
+        //displays all active jobs
+        String body = "";
+        cachedJobs.stream()
+                .forEach(job -> {
+                    body.concat("<p>*********************</p>");
+                    body.concat("<p>Title: " + job.getTitle() + "</p>");
+                    body.concat("<p>Company: " + job.getCompany() + "</p>");
+                    body.concat("<p>IndeedID: " + job.getIndeedId() + "</p>");
+                    body.concat("<p>LINK: <a href=\"" + job.getLink() + "\"/>VISIT</a></p>");
+                    body.concat("<p>*********************</p>");
+                });
+
+        return body;
     }
 }
