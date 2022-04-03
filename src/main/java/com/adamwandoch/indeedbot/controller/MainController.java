854aa66a18e09065ffe8c322b12d655a0621d8b0
Adam Wandoch <wandoch.adam@gmail.com>
2022-04-03 02:41:32 +0100

    mapping tests

--- src/main/java/com/adamwandoch/indeedbot/controller/MainController.java
+++ src/main/java/com/adamwandoch/indeedbot/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.adamwandoch.indeedbot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.adamwandoch.indeedbot.IndeedBotApplication.cachedJobs;
@@ -21,21 +22,22 @@ public class MainController {
     public String jobs() {
         //displays all active jobs
         String body = "";
-        cachedJobs.stream()
-                .filter(job -> job.getTitle().contains("Java"))
-                .forEach(job -> {
-                    body.concat("<p>*********************</p>");
-                    body.concat("<p>Title: " + job.getTitle() + "</p>");
-                    body.concat("<p>Company: " + job.getCompany() + "</p>");
-                    body.concat("<p>IndeedID: " + job.getIndeedId() + "</p>");
-                    body.concat("<p>LINK: " + job.getLink() + "</p>");
-                    body.concat("<p>*********************</p>");
-                });
+//        cachedJobs.stream()
+//                .filter(job -> job.getTitle().contains("Java"))
+//                .forEach(job -> {
+//                    body.concat("<p>*********************</p>");
+//                    body.concat("<p>Title: " + job.getTitle() + "</p>");
+//                    body.concat("<p>Company: " + job.getCompany() + "</p>");
+//                    body.concat("<p>IndeedID: " + job.getIndeedId() + "</p>");
+//                    body.concat("<p>LINK: " + job.getLink() + "</p>");
+//                    body.concat("<p>*********************</p>");
+//                });
+        body = "cachedJobs.size() = " + cachedJobs.size();
         return body;
     }
 
-    @GetMapping("/job")
-    public String job() {
-        return cachedJobs.get(2).getLink();
+    @GetMapping("/job/{index}")
+    public String job(@PathVariable(value = "index") int index) {
+        return cachedJobs.get(index).getTitle();
     }
 }
